860ee9a95ee5adb69364bf4ea3a5bc5a1b13aa3e
Rutvik <rutvikm200@gmail.com>
2023-08-19 09:31:55 +0530

    update name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <Wrapper>
       <div className="nav-center">
-        <span className="logo">Drink Library</span>
+        <span className="logo">Drinks Library</span>
         <div className="nav-links">
           <NavLink to="/" className="nav-link">
             Home
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     <Wrapper>
       <h3>About Us</h3>
       <p>
-        Introducing "Drink Library" the ultimate party sidekick app that fetches
-        cocktails from the hilarious Cocktails DB API. With a flick of your
-        finger, you'll unlock a treasure trove of enchanting drink recipes
+        Introducing "Drinks Library" the ultimate party sidekick app that
+        fetches cocktails from the hilarious Cocktails DB API. With a flick of
+        your finger, you'll unlock a treasure trove of enchanting drink recipes
         that'll make your taste buds dance and your friends jump with joy. Get
         ready to shake up your mixology game, one fantastical mocktail at a
         time, and let the laughter and giggles flow!
